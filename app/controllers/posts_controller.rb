ea9e4d64fdde0e7b72b73db7ba54589416574c5a
steel <hyp.foxtail@gmail.com>
2018-08-26 06:22:25 +0900

    feature blog view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,27 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :is_posted_user?, only: [:edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
   def index
-    # ログインしているユーザの記事を取得
-    @posts = Post.where(user_id: current_user.id)
+    if params[:mode] == 'all'
+      @posts = Post.joins(:user).eager_load(:user)
+      @mode = 'all'
+    else
+      # ログインしているユーザの記事を取得
+      @posts = Post.joins(:user).eager_load(:user).where(user_id: current_user.id)
+      @mode = 'user'
+    end
+    @posts.each do |post|
+      # 表示する記事の文章をはじめの40文字にする
+      post.content = post.content.slice(1, 40)
+    end
+  end
+
+  def index_all
+    @posts = Post.all
   end
 
   # GET /posts/1
@@ -32,7 +47,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html {redirect_to @post, notice: 'Post was successfully created.'}
+        format.html {redirect_to @post, notice: '記事を投稿しました。'}
         format.json {render :show, status: :created, location: @post}
       else
         format.html {render :new}
@@ -46,7 +61,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html {redirect_to @post, notice: 'Post was successfully updated.'}
+        format.html {redirect_to @post, notice: '記事を更新しました。'}
         format.json {render :show, status: :ok, location: @post}
       else
         format.html {render :edit}
@@ -60,7 +75,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html {redirect_to posts_url, notice: 'Post was successfully destroyed.'}
+      format.html {redirect_to posts_url, notice: '記事を削除しました。'}
       format.json {head :no_content}
     end
   end
@@ -75,5 +90,12 @@ class PostsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
     params.require(:post).permit(:user_id, :title, :content)
+  end
+
+  # 不正なエントリ操作はホーム画面にリダイレクト
+  def is_posted_user?
+    if @post.user_id != current_user.id
+      redirect_to :controller => 'home', :action => 'index'
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   validates :title, :presence => true, :length => {:minimum => 5}
   validates :content, :presence => true, :length => {:minimum => 10}
+
+  belongs_to :user
 end
